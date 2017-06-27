057f31d23d44af514ca93772d5ad1b5ebadd6595
annavolkova1 <anna__banana@mail.ru>
2017-06-28 01:40:50 +0300

    add_comments

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+
+# В качестве поля создаём одномерный список с числами от 1 до 9
 board = list(range(1, 10)) # using list() for supporting Python 3
 
 
+# Функция вывода игрового поля
 def draw_board(board):
     print("-------------") # using print() instead of raw_print for supporting Python 3
     for i in range(3):
@@ -9,6 +12,7 @@ def draw_board(board):
         print("-------------")
 
 
+# Функция обработки пользовательского ввода
 def take_input(player_token):
     valid = False
     while not valid:
@@ -28,6 +32,7 @@ def take_input(player_token):
             print("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
 
 
+# Функция проверки игрового поля
 def check_win(board):
     win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
     for each in win_coord:
@@ -36,6 +41,7 @@ def check_win(board):
     return False
 
 
+# Функция объединяющая другие функции в игру
 def main(board):
     counter = 0
     win = False
@@ -58,4 +64,5 @@ def main(board):
     draw_board(board)
 
 
+# Запуск игры
 main(board)
